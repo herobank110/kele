c560a07fde9efb321dce1351fba4f7800b41670d
David Kanekanian <dkanekanian1@gmail.com>
2025-02-09 14:48:10 +0000

    pyside hello world

--- src/kele/__main__.py
+++ src/kele/__main__.py
@@ -1,1 +1,14 @@
-print("hi")
+import sys
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QLabel
+
+
+def main():
+    app = QApplication(sys.argv)
+    label = QLabel("Hello World", alignment=Qt.Alignment.AlignCenter)
+    label.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
